96e18eb4d3b73f1d23b28610c34c3eeb5bf863ae
steftim <steftim.an@gmail.com>
2020-07-17 21:03:58 +0300

    rm some files

--- yandex_api.cpp
+++
@@ -1,6 +0,0 @@
-#include "yandex_api.h"
-
-yandex_api::yandex_api(QObject *parent) : QObject(parent)
-{
-
-}
--- yandex_api.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef YANDEX_API_HPP
-#define YANDEX_API_HPP
-
-#endif // YANDEX_API_HPP
