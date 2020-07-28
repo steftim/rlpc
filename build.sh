137306573f2cfcbc4d9eb8f00187d166e1094b32
steftim <steftim.an@gmail.com>
2020-07-28 21:50:29 +0300

    initial commit

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo cp libyandexmusic.so /usr/lib
-cmake . && make
-./clean.sh
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "rlpcmain.hpp"
-#include <QApplication>
-
-int main(int argc, char *argv[]){
-  QApplication a(argc, argv);
-  rlpcMain w;
-  w.show();
-  return a.exec();
-}
